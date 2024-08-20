60686404592fa3b485edc9ab86fed4f9c343ba80
Gino Filicetti <ginof@google.com>
2024-08-20 16:48:59 -0400

    Added a terraform README and fixed enable-api script

--- scripts/enable-api.sh
+++ scripts/enable-api.sh
@@ -3,40 +3,25 @@
 # Enable IAM permissions for default service accounts for GKE and Cloud Build
 PROJECT_ID=$(gcloud config get-value project)
 
-# Fetch Project Number from Project ID:
-PROJECT_NUMBER=$(gcloud projects describe "$PROJECT_ID" --format="value(projectNumber)")
-
-# GitHub Actions SA:
-GH_ACTIONS_SA="sa-tf-gh-actions"
-
-# 1. Create a Google Cloud Service Account.
-gcloud iam service-accounts create "${GH_ACTIONS_SA}" \
-  --project "${PROJECT_ID}"
-
-# 2. Add roles to Google Cloud Service Account.
-for SUCCINCT_ROLE in \
-    viewer \
-    artifactregistry.admin \
-    cloudbuild.connectionAdmin \
-    cloudbuild.builds.builder \
-    clouddeploy.jobRunner \
-    clouddeploy.releaser \
-    compute.networkAdmin \
-    compute.securityAdmin \
-    container.clusterAdmin \
-    eventarc.developer \
-    iam.serviceAccountAdmin \
-    iam.serviceAccountUser \
-    pubsub.subscriber \
-    resourcemanager.projectIamAdmin \
-    run.developer \
-    run.invoker \
-    storage.admin \
-    storage.objectAdmin \
-    workflows.editor \
-    ; do
-  gcloud projects add-iam-policy-binding \
-    --member="serviceAccount:${GH_ACTIONS_SA}@${PROJECT_ID}.iam.gserviceaccount.com" \
-    --role "roles/$SUCCINCT_ROLE" "$PROJECT_ID" \
-    --condition=None
-done
+# Enable all the APIs needed in our demos
+gcloud services enable --project $PROJECT_ID \
+  aiplatform.googleapis.com \
+  artifactregistry.googleapis.com \
+  batch.googleapis.com \
+  cloudbuild.googleapis.com \
+  cloudresourcemanager.googleapis.com \
+  compute.googleapis.com \
+  container.googleapis.com \
+  containerfilesystem.googleapis.com \
+  containerregistry.googleapis.com \
+  eventarc.googleapis.com \
+  eventarcpublishing.googleapis.com \
+  iam.googleapis.com \
+  pubsub.googleapis.com \
+  run.googleapis.com \
+  servicecontrol.googleapis.com \
+  serviceusage.googleapis.com \
+  storage.googleapis.com \
+  transcoder.googleapis.com \
+  workflows.googleapis.com \
+  workflowexecutions.googleapis.com
--- terraform/README.md
+++ terraform/README.md
@@ -2,4 +2,37 @@
 This folder contains Terraform configuration files to provision the infrastructure needed for this project.
 
 ## Provisioning Infrastructure
-x
+The following steps will walk you through the setup guide for running **Terraform** as the deployment process for cloud environment.
+
+1. Create remote state for Terraform in Google Cloud Storage:
+
+    ```bash
+    sh ./scripts/setup-tfstate.sh
+    ```
+
+2. Create a Terraform tfvars file using your project id to create unique variable names:
+
+    ```bash
+    cd ./terraform
+    cat terraform.example.tfvars | sed -e "s:your-unique-project-id:$PROJECT_ID:g" > terraform.tfvars
+    ```
+
+3. Deploy infrastructure with Terraform
+
+    ```bash
+    cd ./terraform
+    terraform init
+    terraform plan
+    terraform apply
+    ```
+
+> __Note:__ The deployment of cloud resources can take between 5 - 10 minutes.
+
+## Tearing Down Infrastructure
+
+1. Delete infrastructure with Terraform
+
+    ```bash
+    cd ./terraform
+    terraform destroy
+    ```
