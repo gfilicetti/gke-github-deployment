47d739aeb385c4816bd446dac59ece73323a41b3
Gino Filicetti <ginof@google.com>
2024-08-20 16:32:14 -0400

    New README in the terraform folder. Added a script to enable APIs

---
+++ scripts/enable-api.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Enable IAM permissions for default service accounts for GKE and Cloud Build
+PROJECT_ID=$(gcloud config get-value project)
+
+# Fetch Project Number from Project ID:
+PROJECT_NUMBER=$(gcloud projects describe "$PROJECT_ID" --format="value(projectNumber)")
+
+# GitHub Actions SA:
+GH_ACTIONS_SA="sa-tf-gh-actions"
+
+# 1. Create a Google Cloud Service Account.
+gcloud iam service-accounts create "${GH_ACTIONS_SA}" \
+  --project "${PROJECT_ID}"
+
+# 2. Add roles to Google Cloud Service Account.
+for SUCCINCT_ROLE in \
+    viewer \
+    artifactregistry.admin \
+    cloudbuild.connectionAdmin \
+    cloudbuild.builds.builder \
+    clouddeploy.jobRunner \
+    clouddeploy.releaser \
+    compute.networkAdmin \
+    compute.securityAdmin \
+    container.clusterAdmin \
+    eventarc.developer \
+    iam.serviceAccountAdmin \
+    iam.serviceAccountUser \
+    pubsub.subscriber \
+    resourcemanager.projectIamAdmin \
+    run.developer \
+    run.invoker \
+    storage.admin \
+    storage.objectAdmin \
+    workflows.editor \
+    ; do
+  gcloud projects add-iam-policy-binding \
+    --member="serviceAccount:${GH_ACTIONS_SA}@${PROJECT_ID}.iam.gserviceaccount.com" \
+    --role "roles/$SUCCINCT_ROLE" "$PROJECT_ID" \
+    --condition=None
+done
---
+++ terraform/README.md
@@ -0,0 +1,5 @@
+# Terraform
+This folder contains Terraform configuration files to provision the infrastructure needed for this project.
+
+## Provisioning Infrastructure
+x
