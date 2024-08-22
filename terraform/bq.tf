691796b6115a4aadf8dd448f21c9972ce15b0510
Alan Poole <alanpoole@google.com>
2024-08-22 11:24:22 -0500

    Add job summary view in BQ.

--- terraform/bq.tf
+++ terraform/bq.tf
@@ -20,24 +20,26 @@ module "bigquery" {
   description  = "Each row of this table represents a Transcoder Job initiated by an upload to the Google Cloud Storage (GCS) bucket or by bulk upload in the Workflow UI."
   project_id   = var.project_id
   location     = var.region
-  tables = [
-    {
-      table_id           = "jobs",
-      schema             = file("../analytics/bq-job-stats-schema.json"),
-      clustering         = null
-      expiration_time    = null
-      range_partitioning = null
-      time_partitioning  = null
-      labels = {
-        env = "transcoding"
-      },
-    },
-  ]
   dataset_labels = {
     env = "transcoding"
   }
 }
 
+# Create a BigQuery Table to house job metadata from the Workflow
+resource "google_bigquery_table" "jobs" {
+  dataset_id = module.bigquery.bigquery_dataset.dataset_id
+  table_id   = "jobs"
+  schema     = file("../analytics/bq-job-stats-schema.json")
+  labels = {
+    env = "transcoding"
+  }
+
+  depends_on = [
+    module.bigquery
+  ]
+}
+
+# A BigQuery connection to the rest of the GCP Resources
 resource "google_bigquery_connection" "cloud_resource_connection" {
   connection_id = "bq-biglake-gcp-resources"
   location      = var.region
@@ -94,5 +96,52 @@ resource "google_bigquery_table" "gcs_objects_output" {
   # before Terraform creates a table.
   depends_on = [
     google_project_iam_member.bigquery_sa_objects
+  ]
+}
+
+# A view that combines Job statistics, input, and output information
+resource "google_bigquery_table" "job-stats-summary" {
+  dataset_id = module.bigquery.bigquery_dataset.dataset_id
+  table_id   = "job-stats-summary"
+
+  view {
+    use_legacy_sql = false
+    query = <<EOF
+    # Query that combines Job stats information with GCS object metadata and transcoding backend logs.
+    SELECT 
+      j.JobId,
+      j.createdDateTime,
+      j.BackendSrv,
+      input.URI as input_file_uri,
+      input.generation as input_file_generation,
+      input.content_type as input_file_content_type,
+      input.size as input_file_content_size,
+      output.URI as output_file_uri,
+      output.generation as outputfile_generation,
+      output.content_type as output_file_content_type,
+      output.size as output_file_content_size,
+    FROM `${module.bigquery.bigquery_dataset.dataset_id}.${google_bigquery_table.jobs.table_id}` AS j
+    LEFT JOIN `${module.bigquery.bigquery_dataset.dataset_id}.${google_bigquery_table.gcs_objects_input.table_id}` AS input 
+      ON j.FileURI = input.URI
+    LEFT JOIN (
+      SELECT
+        URI,
+        SPLIT(URI, "/") AS URI_PARTS,
+        generation,
+        content_type,
+        size
+      FROM
+        `${module.bigquery.bigquery_dataset.dataset_id}.${google_bigquery_table.gcs_objects_output.table_id}`
+      ) AS output
+    ON
+      output.URI_PARTS[SAFE_OFFSET(2)] = "${resource.google_storage_bucket.gcs-output.name}"
+      AND output.URI_PARTS[SAFE_OFFSET(3)] = j.BackendSrv
+      AND output.URI_PARTS[SAFE_OFFSET(4)] = j.JobId
+    EOF
+  }
+
+  depends_on = [
+    google_bigquery_table.jobs,
+    google_bigquery_table.gcs_objects_input
   ]
 }
