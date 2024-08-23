a166981c7e088b088da834edca0952e8214b8715
Gino Filicetti <ginof@google.com>
2024-08-23 14:16:52 -0400

    Terraform commands can only be run from the terraform folder, and we were cd'ing to the gke folder prematurely

--- gke/scripts/setup-gke.sh
+++ gke/scripts/setup-gke.sh
@@ -36,9 +36,6 @@ else
   terraform apply "out.tfplan"
 fi
 
-# Get back into the GKE folder
-cd ../gke
-
 # Replace variables in GKE configuration files
 # Get Terraform output in json format
 TF_OUTPUT_JSON=`terraform output -json`
@@ -47,16 +44,19 @@ TF_CLUSTER=`jq -r '.gke_name.value' <<< $TF_OUTPUT_JSON`
 TF_REGION=`jq -r '.region.value' <<< $TF_OUTPUT_JSON`
 TF_PROJECT=`jq -r '.project_id.value' <<< $TF_OUTPUT_JSON`
 
+# Get back into the GKE folder
+cd ../gke
+
 # Test if our outputs are good by trying to get k8s creds for our cluster
 if gcloud container clusters get-credentials $TF_CLUSTER --region $TF_REGION --project $TF_PROJECT; then
   # Create an array of comma separated key value pairs in the bash array called "outputs"
   readarray -t outputs <<< `jq -r 'to_entries[] | "\(.key),\(.value.value)"' <<< $TF_OUTPUT_JSON`
 
-  # Loop through each output, grep for the key in all files and replace it with the value if found
+  # Loop through each output, grep for the key in all GKE config files and replace it with the value if found
   for output in ${outputs[@]}; do
     key=`awk -F',' '{print $1}' <<< "$output"`
     value=`awk -F',' '{print $2}' <<< "$output"`
-    for file in `grep $key -rl ../gke/*`; do
+    for file in `grep $key -rl *`; do
       sed -i -e 's:$key:$value:g' $file
     done
   done
