90ccfd5501061aee06364b5fb46dde917828d49f
Michael Bychkowski <mbychkowski@google.com>
2024-09-13 07:44:34 +0000

    undo flags for transcoding infra

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -26,13 +26,3 @@ variable "job_namespace" {
   type        = string
   default     = "jobs"
 }
-
-variable "transcoding_method" {
-  description = "Flags to enable specific installation methods"
-  type = map(bool)
-  default = {
-    "enable_gke_jobs"         = true
-    "enable_transcoding_api" = true
-    "enable_gce_batch"       = true
-  }
-}
